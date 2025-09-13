e1dd80488643411af4c5c33d74b44a339f53d8df
Himanshu Pandey <himanshupandey2003@gmail.com>
2025-09-13 09:22:45 +0530

    changing in app.js of correcting cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const app=express();
 
 
 app.use(cors({
-  origin: 'http://localhost:8080', // Your frontend URL
+  origin: '*', // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
